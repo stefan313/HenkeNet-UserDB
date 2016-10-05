b966f75b852f351ac7c3dc04f508a5d31eed77fc
stefan313 <stefan.schoeninger@fau.de>
2016-10-05 15:03:53 +0200

    test commit, removed unused imports

--- MainView.java
+++ MainView.java
@@ -5,11 +5,8 @@
  */
 import java.util.List;
 import java.util.logging.Logger;
-import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JPasswordField;
 import javax.swing.JTable;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
