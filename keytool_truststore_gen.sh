f55884061912578de25e1720225011a7e8f2ba4d
stefan313 <stefan.schoeninger@fau.de>
2016-10-17 15:06:10 +0200

    keytool

--- keytool_truststore_gen.sh
+++ keytool_truststore_gen.sh
@@ -12,21 +12,24 @@ else
 	exit 1
 fi
 
+keystore = "keystore"
+truststore = "truststore"
+
 echo "generating truststore for UserTool with ${1}!"
 read -p "PW for truststore:" truststorepw
 # https://dev.mysql.com/doc/connector-j/5.1/en/connector-j-reference-using-ssl.html
-keytool -v -import -alias mysqlServerCACert -file ${1} -keystore truststore
+keytool -v -import -alias mysqlServerCACert -file ${1} -keystore ${truststore}
 
 echo "keytool has created truststore"
 echo "creating keystore for Usertool, geneating new key"
 read -p "PW for keystore:" keystorepw
-keytool -v -genkey -keyalg rsa -alias mysqlClientCert -keystore keystore
+keytool -v -genkey -keyalg rsa -alias mysqlClientCert -keystore ${keystore}
 
 echo "adding files to your config, CAUTION!: absolute Paths!"
 
 # Absoluter Pfad für keystores!
-$pwd = pwd
-echo "keyStorePath=${pwd}/keystore" >> configuration.txt
+workingdir = pwd
+echo "keyStorePath=${workingdir}/${keystore}" >> configuration.txt
 echo "keyStorePassword=${keystorepw}" >> configuration.txt
-echo "trustStorePath=${pwd}/truststore" >> configuration.txt
+echo "trustStorePath=${workingdir}/${truststore}" >> configuration.txt
 echo "trustStorePassword=${truststorepw}" >> configuration.txt
